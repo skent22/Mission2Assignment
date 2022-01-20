a7b62d97be8fffab5dc44c5ee52d5035b64cd482
skent22 <90283433+skent22@users.noreply.github.com>
2022-01-19 17:16:00 -0700

    Mission3

---
+++ Mission2Assignment/Models/GradeCalculatorModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        [Range(0, 100)] public double Assignment { get; set; }
+        [Range(0, 100)] public double Group { get; set; }
+        [Range(0, 100)] public double Quiz { get; set; }
+        [Range(0, 100)] public double Exam { get; set; }
+        [Range(0, 100)] public double INTEX { get; set; }
+    }
+}
